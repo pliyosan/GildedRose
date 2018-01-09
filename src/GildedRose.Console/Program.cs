e953df2ce8b9272547c24e001ff0578d44790709
Juan Cerezuela <juanjo.cerezuela@asos.com>
2018-01-09 16:12:52 +0000

    Fixing payments

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -17,7 +17,7 @@ namespace GildedRose.Console
                               Items = new List<Item>
                                           {
                                               new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "New Brie 2", SellIn = 2, Quality = 0},
+                                              new Item {Name = "Aged Cheese", SellIn = 2, Quality = 0},
                                               new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                                               new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
                                               new Item
